b29b637a058b8345b0cd145fa002e505528f3018
Jonathan McKendry <52171140+McKendrigo@users.noreply.github.com>
2020-11-11 16:27:52 +0000

    Custom setup bug fix
    Custom setup importing should now hopefully generate starting angles without bugs now.

--- Custom_setup.m
+++ Custom_setup.m
@@ -4,17 +4,22 @@
 
 % Imported data should be formated as .csv, column 1 = angle (degress),
 % column 2 = relative intensity (0 to 1).
+% Final entries should always be 90degrees = 0.
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [source_qfit] = Custom_setup(theta_degrees,profile)
-theta = deg2rad(theta_degrees);
-intensity = (profile.*sin(theta))/pi;
-int_fit = fit(theta,intensity,'linearinterp');
+function [source_qfit,q] = Custom_setup(theta_degrees,profile)
+theta = deg2rad(theta_degrees); % Convert degrees to radians
+int_fit = fit(theta,profile,'linearinterp'); % Interpolate imported data
+fun = @(x)(int_fit(x)/pi).*sin(x); % Fit function (intensity/solid angle)
 
+q = zeros(length(theta),1); % Pre-allocate array for speed
 index = 1;
 while index < length(theta)+1
-    q(:,index) = 2*pi*integrate(int_fit,theta(index),0); % Integrate to find relationship between q and emission angle, theta
+    q(index,:) = 2*pi*integral(fun,0,theta(index),'ArrayValued',true); % Integrate to find relationship between q and emission angle, theta
     index = index + 1;
 end
 
-source_qfit = fit(transpose(q),theta,'linearinterp'); % Fit to pass to the rest of the script
+q(end) = 1; % Constrain output so that q=1 at 90 degrees
+
+source_qfit = fit(q,theta,'linearinterp'); % Fit to pass to the rest of the script
