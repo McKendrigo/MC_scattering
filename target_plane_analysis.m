f48d1d8f9814f898e47bf5c288dacf6fe0635215
Jonathan McKendry <52171140+McKendrigo@users.noreply.github.com>
2021-01-07 16:20:48 +0000

    Version1.1
    Rx target sorted. All analysis can now be done in any plane. Location of target Rx and target grid can now be defined in terms of their corner coordinates, rather than being fixed to being centred at "0,0".

--- target_plane_analysis.m
+++ target_plane_analysis.m
@@ -15,9 +15,8 @@
 
 % 1) Setting up
 %%%%%%%%%%%%%%%
-function [Rx_received_total,xGrid,yGrid,zGrid,weightMatrix] = target_plane_analysis(coordinates,hitweights,trgt_corners,trgt_plane,grid_num,Rx_previous,weightMatrix_previous,savetype,save_yn,outputfilename)
+function [Rx_received_total,xGrid,yGrid,zGrid,weightMatrix] = target_plane_analysis(coordinates,hitweights,trgt_corners,trgt_plane,grid_num,Rx_coordinates,Rx_previous,weightMatrix_previous,savetype,save_yn,outputfilename)
 
-   
 xmin = trgt_corners(1);
 xmax = trgt_corners(2);
 ymin = trgt_corners(3);
@@ -103,16 +102,23 @@ if isempty(grid_hits) == 0
 
     % 3) Find all packets within target area and sum their packet weights
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Rx_xmin = Rx_coordinates(1);
+    Rx_xmax = Rx_coordinates(2);
+    Rx_ymin = Rx_coordinates(3);
+    Rx_ymax = Rx_coordinates(4);
+    Rx_zmin = Rx_coordinates(5);
+    Rx_zmax = Rx_coordinates(6);
+    
     switch trgt_plane
         case 'x'
-        target_hits = find(coordinates(:,2) >= ymin & coordinates(:,2) <= ymax...
-            & coordinates(:,3) >= zmin & coordinates(:,3) <= zmax);
+        target_hits = find(coordinates(:,2) >= Rx_ymin & coordinates(:,2) <= Rx_ymax...
+            & coordinates(:,3) >= Rx_zmin & coordinates(:,3) <= Rx_zmax);
         case 'y'
-        target_hits = find(coordinates(:,1) >= xmin & coordinates(:,1) <= xmax...
-            & coordinates(:,3) >= zmin & coordinates(:,3) <= zmax);
+        target_hits = find(coordinates(:,1) >= Rx_xmin & coordinates(:,1) <= Rx_xmax...
+            & coordinates(:,3) >= Rx_zmin & coordinates(:,3) <= Rx_zmax);
         case 'z'
-        target_hits = find(coordinates(:,1) >= xmin & coordinates(:,1) <= xmax...
-            & coordinates(:,2) >= ymin & coordinates(:,2) <= ymax);
+        target_hits = find(coordinates(:,1) >= Rx_xmin & coordinates(:,1) <= Rx_xmax...
+            & coordinates(:,2) >= Rx_ymin & coordinates(:,2) <= Rx_ymax);
     end
 
     Rx_received_total = sum(hitweights(target_hits)); % This is the total sum of packets hitting the receiver
@@ -126,11 +132,11 @@ end
 if save_yn == "yes"
     if savetype == "Results only"
     save(outputfilename,'xGrid','yGrid','zGrid','trgt_corners', ...
-    'trgt_plane','Rx_received_total','weightMatrix'); % Save output
+    'trgt_plane','Rx_received_total','weightMatrix','Rx_coordinates'); % Save output
     elseif savetype == "All"
     save(outputfilename,'coordinates','hitweights','xGrid','yGrid', ...
     'zGrid','Rx_received_total','weightMatrix',...
-    'trgt_corners','trgt_plane'); % Save output
+    'trgt_corners','trgt_plane','Rx_coordinates'); % Save output
     end
 end
 
